53d7e9bfd0fa8638b86be275810df4e6afb1a2c0
AidanFromProgramming <46631427+AidanFromProgramming@users.noreply.github.com>
2020-07-24 22:10:22 -0400

    Did a lot

--- venv/systemUtilitys.py
+++ venv/systemUtilitys.py
@@ -54,8 +54,9 @@ def getStatus(load, ram, configSize, heartbeat, ping, cache, maxCache):
         level += 1
         problems.append("High Memory Usage")
     if configSize > 1250:
-        level += 1
-        problems.append("High Config Usage")
+        #level += 1
+        #problems.append("High Config Usage")
+        pass
     if cache > maxCache/ 1.5:
         level += 1
         problems.append("High Cache Usage")
